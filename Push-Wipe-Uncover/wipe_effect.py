903c8fc88258cd5d07edb07ca0f6e87f15f022e6
Erwin2409 <83054910+erwin24092002@users.noreply.github.com>
2022-03-18 15:31:30 +0700

    upload

---
+++ Push-Wipe-Uncover/wipe_effect.py
@@ -0,0 +1,28 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+slider1 = cv2.imread('slider1.jpg', 1)
+slider2 = cv2.imread('slider2.jpg', 1)
+img = cv2.cvtColor(slider1, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(slider2, cv2.COLOR_BGR2RGB)
+
+# Chuẩn hóa kích thước của ảnh
+# Lấy kích thước của ảnh:
+h1,w1,c1 = slider1.shape
+h2,w2,c2 = slider2.shape
+# Lưu vào h chiều cao, w chiều rộng nhỏ nhất giữa 2 ảnh:
+h = min(h1,h2)
+w = min(w1,w1)
+# Thay đổi kích thước ảnh theo w,h:
+img1 = cv2.resize(slider1,(w,h))
+img2 = cv2.resize(slider2,(w,h))
+
+# Xuất kết quả
+result = []
+stride = 2
+for D in range(0, w+1, stride):
+    result = img1.copy()
+    result[:, 0:D, :] = img2[:, 0:D, :]
+    cv2.imshow("Wipe Effect", result)
+    cv2.waitKey(5)
