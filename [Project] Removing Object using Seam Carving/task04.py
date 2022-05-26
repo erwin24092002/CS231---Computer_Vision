873ce6006ef7da764636e2fbbc78d19cd13d9935
Trần Văn Lực <99704547+VanlucCS@users.noreply.github.com>
2022-05-26 22:18:56 +0700

    update

--- [Project] Removing Object using Seam Carving/task04.py
+++ [Project] Removing Object using Seam Carving/task04.py
@@ -1,8 +1,27 @@
-def hello(arg):
+def get_minimum_seam(self,emap):
     """_summary_
     Args:
-        arg (type): _description_. Defaults to __.
+        array (h x w): matrix energy map.
 
     Returns:
-        _type_: _description_
+        array (h): a array has index of minimum energy seam of energy map
     """
+    # Generate seam map
+    smap = self.gen_smap(emap) 
+    
+    # Get seam
+    seam = []
+    h, w = smap.shape
+    index = np.argmin(smap[h-1, :])
+    seam.append(index)
+
+    # get the index of the pixel that has a minimum seam energy per row in the seam map 
+    for i in range(h-1, 0, -1):
+        if index == 0:  # If the index of the below line == 0, we only consider min from index to index +1
+            index = index + np.argmin(smap[i, index:index+2])
+        elif index == w-1:# If the index of the below line == w -1, we only consider min from index-1 to index 
+            index = index - 1 +  np.argmin(smap[i, index-1:index+1])
+        else: # else we only consider min from index-1 to index +1
+            index = index - 1 + np.argmin(smap[i, index-1:index+2])
+        seam.append(index) # add idex of pixel has minimun seam energy
+        
--- [Project] Removing Object using Seam Carving/task05.py
+++ [Project] Removing Object using Seam Carving/task05.py
@@ -1,8 +1,20 @@
-def hello(arg):
+def remove_seam(self,seam):
     """_summary_
     Args:
-        arg (type): _description_. Defaults to __.
+        array (h): a array has index of minimum energy seam of energy map.
 
     Returns:
-        _type_: _description_
+        array (h x w-1 x c): an image after remove the minimun seam 
     """
+
+    h, w, c = self.new_img.shape
+    new_img = self.new_img.copy()
+    mask = np.ones((h, w), dtype=np.bool_)
+    # get mask has minimun seam to remove
+    for i in range(0,h):
+        mask[i, seam[i]] = False # mark pixel remove on mask
+
+    mask = np.stack([mask] * 3, axis=2)
+    # remove the seam 
+    new_img = new_img[mask].reshape((h, w - 1, 3))
+    return new_img
